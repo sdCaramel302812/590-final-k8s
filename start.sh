27ccc156c3ee67970971e8d751ec3d58fe9b40ac
sdCaramel302812 <kypss93097@gmail.com>
2022-04-08 17:39:52 -0400

    first commit

---
+++ start.sh
@@ -0,0 +1,34 @@
+if ! command -v docker &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
+fi
+
+if ! command -v minikube &> /dev/null; then 
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+    sudo dpkg -i minikube_latest_amd64.deb
+fi
+
+if ! command -v kubectl &> /dev/null; then
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+fi
+
+dockerd
+minikube start
+
+kubectl apply -f be-deployment.yaml
+kubectl apply -f fe-deployment.yaml
+kubectl apply -f be-service.yaml
+kubectl apply -f fe-service.yaml
+
+kubectl get service
